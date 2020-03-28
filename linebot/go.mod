3c7ff705d3b4e611781f02196d5edb196d93794c
Kota Aoki <bluemon0919@gmail.com>
2020-03-28 17:12:47 +0900

    ulidの取り込み

--- linebot/go.mod
+++ linebot/go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/line/line-bot-sdk-go v7.3.0+incompatible
-	github.com/oklog/ulid/v2 v2.0.2 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/oklog/ulid/v2 v2.0.2
+	golang.org/x/tools/gopls v0.3.4 // indirect
 )
--- linebot/session.go
+++ linebot/session.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/oklog/ulid"
+)
+
 // UserSession manages linebot users session
 type UserSession struct {
 	UserID string
@@ -26,4 +34,12 @@ func (us *UserSession) Close(userID string) {
 	if us.Count > 3 {
 		us.Count = 0
 	}
+}
+
+func createSessionID() {
+	t := time.Now()
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	id := ulid.MustNew(ulid.Timestamp(t), entropy)
+
+	fmt.Println(id)
 }
